f9f1d0b256326cc16a7c6a9439709b008dbf42ed
Rithish06 <rithish.manohar006@gmail.com>
2025-01-07 17:48:01 +0530

    Rashtrotthana orthopedics page seo

--- src/app/orthopedics/orthopedics.component.ts
+++ src/app/orthopedics/orthopedics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title, Meta,DomSanitizer,SafeHtml } from '@angular/platform-browser'; 
+import { Title, Meta, DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 
 @Component({
@@ -9,48 +9,97 @@ import { Title, Meta,DomSanitizer,SafeHtml } from '@angular/platform-browser';
 })
 export class OrthopedicsComponent {
   constructor(private titleService: Title, private metaService: Meta, private sanitizer: DomSanitizer) {
-    
+
   }
-  sanitizedContent: SafeHtml ='';
+  sanitizedContent: SafeHtml = '';
   sanitizedContent1: SafeHtml = '';
-  specialities:any[] = [];  
+  specialities: any[] = [];
   ngOnInit(): void {
-    this.titleService.setTitle("Best orthopaedics Hospital in Bangalore, India - Rashtrotthana Hospital");  
+    this.titleService.setTitle("Best orthopaedics Hospital in Bangalore, India - Rashtrotthana Hospital");
 
-  this.metaService.updateTag({ name: 'description', content: 'Rashtrotthana Hospital is the best orthopaedics hospital in Bangalore, India with highly experienced orthopaedics/ortho doctor providing orthopaedic treatment & surgeries.' });
+    this.metaService.updateTag({ name: 'description', content: 'Rashtrotthana Hospital is the best orthopaedics hospital in Bangalore, India with highly experienced orthopaedics/ortho doctor providing orthopaedic treatment & surgeries.' });
 
-  this.metaService.updateTag({ name: 'keywords', content: 'orthopaedics hospital near me, orthopaedics, knee, knee pain, shoulder pain, bone fractures, joint pains, ortho, knee pain causes' });
+    this.metaService.updateTag({ name: 'keywords', content: 'orthopaedics hospital near me, orthopaedics, knee, knee pain, shoulder pain, bone fractures, joint pains, ortho, knee pain causes' });
 
-  this.specialities=[
-    {
-      main_heading:'Orthopedics',
-      content:`The Orthopedics Department at Rashtrotthana Hospital is dedicated to providing comprehensive care for bone, joint and muscle conditions, supported by a team of highly skilled orthopaedic specialists. With a patient-centered approach, our team is experienced in diagnosing and treating a broad spectrum of musculoskeletal disorders, from arthritis and sports injuries to fractures and degenerative joint conditions. Our department offers specialized treatments and surgeries, including arthroscopy, <a href="https://en.wikipedia.org/wiki/Joint_replacement">joint replacement </a>and minimally invasive spine surgery. Our advanced facilities and state-of-the-art technology allow us to deliver safe, effective and minimally invasive procedures that support faster recovery and long-term outcomes for patients.`,
-      content_1:'With a strong emphasis on quality, Rashtrotthana Hospital has become a trusted centre for knee and hip replacement surgeries, achieving consistently high success rates. Our Accident & Trauma Care Unit is available 24/7, ensuring immediate, high-quality care for critical injuries. We offer a full range of services tailored to meet individual needs, from preoperative evaluations to comprehensive post-surgery rehabilitation. Whether it’s restoring mobility, alleviating pain, or enhancing quality of life, our Orthopedics Department is committed to delivering compassionate and high-standard orthopaedic care in Bangalore.',
-      heading:'Orthopedics',
-      image:'best_orthopedics_treatment_in_bengaluru.png',
-      alt:'Best Orthopedics Treatment in Bengaluru',
-      Doctors:[
-        {
-          doctor_image:'../../assets/Dr-Mahesh-Kulkarni.png',
-          doctor_name:'Dr. Mahesh Kulkarni',
-          experience : "15+",
-          docalt : 'Dr. Mahesh Kulkarni | Best Ortho Doctor in Bangalore | Rashtrotthana Hospital'
-        },
-        {
-          doctor_image:'../../assets/Dr-Sujayendra-D-M.png',
-          doctor_name:'Dr. Sujayendra D. M',
-          experience : "11",
-          docalt : 'Dr. Sujayendra D. M | Best Orthopaedic Doctor in Bangalore | Rashtrotthana Hospital'
-        },{
-        
-        doctor_image:'../../assets/Dr-Nikhil-Hegde.png',
-        doctor_name:'Dr. Nikhil Hegde',
-        experience : "6",
-        docalt : 'Dr. Nikhil Hegde | Best Orthopaedic Consultant in Bangalore | Rashtrotthana Hospital'
-      }
-      ]}];
-      this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content);
-        this.sanitizedContent1 = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content_1);
+
+    this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content);
+    this.sanitizedContent1 = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content_1);
   }
+
+  doctors = [
+    {
+      doctor_image: '../../assets/Dr-Mahesh-Kulkarni.png',
+      doctor_name: 'Dr. Mahesh Kulkarni',
+      experience: "15+",
+      docalt: 'Dr. Mahesh Kulkarni | Best Ortho Doctor in Bangalore | Rashtrotthana Hospital'
+    },
+    {
+      doctor_image: '../../assets/Dr-Sujayendra-D-M.png',
+      doctor_name: 'Dr. Sujayendra D. M',
+      experience: "11",
+      docalt: 'Dr. Sujayendra D. M | Best Orthopaedic Doctor in Bangalore | Rashtrotthana Hospital'
+    }, {
+
+      doctor_image: '../../assets/Dr-Nikhil-Hegde.png',
+      doctor_name: 'Dr. Nikhil Hegde',
+      experience: "6",
+      docalt: 'Dr. Nikhil Hegde | Best Orthopaedic Consultant in Bangalore | Rashtrotthana Hospital'
+    }
+  ]
+
+  faqs = [
+    {
+      ques : "What services does the Best Orthopedic Hospital in RR Nagar Bangalore offer?",
+      ans : "At Rashtrotthana Hospital, recognized as the Best Orthopedic Hospital in RR Nagar Bangalore, we provide comprehensive services including joint replacement surgeries, minimally invasive spine surgeries, arthroscopy, fracture care and specialized treatments for various musculoskeletal conditions."
+    },
+    {
+      ques : "How can I find an Orthopedic Specialist in RR Nagar?",
+      ans : "You can find experienced Orthopedic Specialists in RR Nagar at Rashtrotthana Hospital. Our team comprises highly qualified professionals dedicated to providing personalized care for all orthopedic concerns."
+    },
+    {
+      ques : "Who are the Best Orthopedic Surgeons in RR Nagar?",
+      ans : "Rashtrotthana Hospital boasts some of the Best Orthopedic Surgeons in RR Nagar, renowned for their expertise in performing complex orthopedic surgeries with high success rates."
+    },
+    {
+      ques : "What conditions do Bone Specialists in RR Nagar treat?",
+      ans : "Our Bone Specialists in RR Nagar diagnose and treat a wide range of conditions, including fractures, osteoporosis, bone infections and tumors, ensuring comprehensive bone health management."
+    },
+    {
+      ques : "How do I locate the Best Orthopedic Hospital near me?",
+      ans : "If you're searching for the Best Orthopedic Hospital near you, Rashtrotthana Hospital in RR Nagar is a leading choice, offering state-of-the-art facilities and a team of expert orthopedic professionals."
+    },
+    {
+      ques : "Where can I find the Best Orthopedic Doctors near me?",
+      ans : "Rashtrotthana Hospital in RR Nagar is home to some of the Best Orthopedic Doctors near you, providing exceptional care for all orthopedic conditions."
+    },
+    {
+      ques : "How can I consult an orthopedic doctor near me?",
+      ans : "To consult an orthopedic specialist near you, visit Rashtrotthana Hospital in RR Nagar, where our team is ready to assist with your musculoskeletal health needs."
+    },
+    {
+      ques : "What should I consider when choosing an orthopedic surgeon?",
+      ans : "When selecting an orthopedic surgeon, consider their qualifications, experience, specialization and patient reviews. Rashtrotthana Hospital's orthopedic surgeons are highly esteemed in these aspects."
+    },
+    {
+      ques : "How do I find an orthopedic surgeon near me?",
+      ans : "For those seeking an orthopedic surgeon nearby, Rashtrotthana Hospital in RR Nagar offers access to top-tier surgeons specializing in various orthopedic procedures."
+    },
+    {
+      ques : "What does a bone doctor do?",
+      ans : "A bones doctor, or orthopedic specialist, diagnoses and treats conditions related to the skeletal system, including fractures, bone diseases and joint disorders."
+    },
+    {
+      ques : "Who is the Best Knee Replacement Surgeon in RR Nagar?",
+      ans : "Rashtrotthana Hospital is proud to have some of the Best Knee Replacement Surgeons in RR Nagar, offering advanced surgical techniques for optimal patient outcomes."
+    },
+    {
+      ques : "What makes Rashtrotthana Hospital the Best Bone Hospital in RR Nagar Bangalore?",
+      ans : "Rashtrotthana Hospital is considered the Best Bone Hospital in RR Nagar Bangalore due to our comprehensive bone health services, advanced technology and a team of expert specialists dedicated to patient care."
+    },
+    {
+      ques : "Which is the Best Bone Surgery Hospital in RR Nagar Bangalore?",
+      ans : "Rashtrotthana Hospital stands out as the Best Bone Surgery Hospital in RR Nagar Bangalore, equipped with state-of-the-art surgical facilities and a team of highly skilled surgeons."
+    },
+  ]
 
 }
---
+++ src/app/speciality-doctors/speciality-doctors.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-speciality-doctors',
+  templateUrl: './speciality-doctors.component.html',
+  styleUrl: './speciality-doctors.component.css'
+})
+export class SpecialityDoctorsComponent {
+  
+  @Input()name:string='';
+  @Input()experience:string='';
+  @Input()image:string='';
+  @Input()alt:string='';
+}
