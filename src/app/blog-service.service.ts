280882e07ff653b215c6af9d0bf0772fba11d447
keerthana <keerthana.m@iamneo.ai>
2025-03-13 20:38:42 +0530

    ayuverda

--- src/app/blog-service.service.ts
+++ src/app/blog-service.service.ts
@@ -11,7 +11,7 @@ export class BlogServiceService {
   constructor(private http: HttpClient) { }
 
   getPosts(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/posts?per_page=20`);
+    return this.http.get(`${this.baseUrl}/posts?per_page=100`);
   }
 
   // getPost(id: number): Observable<any> {
