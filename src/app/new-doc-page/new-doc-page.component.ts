30dfed76c131e1a561428a6d7e0d94e4ac21005a
Rithish06 <rithish.manohar006@gmail.com>
2025-05-07 10:35:33 +0530

    binded

--- src/app/new-doc-page/new-doc-page.component.ts
+++ src/app/new-doc-page/new-doc-page.component.ts
@@ -15,6 +15,44 @@ export class NewDocPageComponent {
 
   }
 
-  
+  doctorDetails : any = [
+    {
+      name : "Dr.(Col)Anand Shankar",
+      qualification : "MBBS,MD (ANAESTHESIOLOGY), EDIC",
+      department : "ER HEAD, ICU, ANAESTHESIA",
+      experience : "31",
+      image : "../../assets/Dr-Col-Anand-Shankar-new.png",
+      alt : "Best Anesthesiology Doctor RR Nagar Bangalore",
+      briefProfile : `Col (Dr) Anand Shankar K is an alumnus of the prestigious Armed Forces Medical
+                College, Pune. He served in field areas prior to pursuing postgraduation in the field of Anaesthesiology
+                and Intensive Care at AFMC pune. Thereafter he qualified the European Diploma in Intensive Care
+                Medicine. He has an overall experience of more than 31 years, 27 of those years serving with the Armed
+                Forces. He has served within the country and in foreign missions with the United Nations. He also had
+                the unique opportunity to serve as accompanying doctor to the President of India. He has been serving
+                with Rashtrotthana Hospital since its inception in Dec 2022.`,
+      areaOfExpertise1 : ['GI surgery', 'Lasers', 'Laproscopy', 'Onco Surgery', 'Lasers'],
+      areaOfExpertise2 : ['GI surgery', 'Lasers', 'Laproscopy', 'Onco Surgery', 'Lasers'],
+      metaTitle : "",
+      metaDescription : ""
+    }
+  ]
+
+  filterdDoctor : any = {
+    name : "Dr.(Col)Anand Shankar",
+    qualification : "MBBS,MD (ANAESTHESIOLOGY), EDIC",
+    department : "ER HEAD, ICU, ANAESTHESIA",
+    experience : "31",
+    image : "../../assets/Dr-Col-Anand-Shankar-new.png",
+    alt : "Best Anesthesiology Doctor RR Nagar Bangalore",
+    briefProfile : `Col (Dr) Anand Shankar K is an alumnus of the prestigious Armed Forces Medical
+              College, Pune. He served in field areas prior to pursuing postgraduation in the field of Anaesthesiology
+              and Intensive Care at AFMC pune. Thereafter he qualified the European Diploma in Intensive Care
+              Medicine. He has an overall experience of more than 31 years, 27 of those years serving with the Armed
+              Forces. He has served within the country and in foreign missions with the United Nations. He also had
+              the unique opportunity to serve as accompanying doctor to the President of India. He has been serving
+              with Rashtrotthana Hospital since its inception in Dec 2022.`,
+    areaOfExpertise1 : ['GI surgery', 'Lasers', 'Laproscopy', 'Onco Surgery', 'Lasers'],
+    areaOfExpertise2 : ['GI surgery', 'Lasers', 'Laproscopy', 'Onco Surgery', 'Lasers'],
+  }
 
 }
