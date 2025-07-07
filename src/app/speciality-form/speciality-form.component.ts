0f91583eb6bdd2dcee111a7499ebabb00978d5e1
keerthana <keerthana.m@iamneo.ai>
2025-07-07 16:01:22 +0530

    form-backend-integration

--- src/app/speciality-form/speciality-form.component.ts
+++ src/app/speciality-form/speciality-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-speciality-form',
@@ -11,7 +13,8 @@ export class SpecialityFormComponent {
   @Input() doctor : any
 
   appointmentForm!: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  apiUrl:string= 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api/email'
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
     this.appointmentForm = this.fb.group({
@@ -27,6 +30,22 @@ export class SpecialityFormComponent {
   onSubmit(): void {
     if (this.appointmentForm.valid) {
       console.log('Form Submitted:', this.appointmentForm.value);
+      const emailRequest = {
+        to: "patientservices@rashtrotthanahospital.com",
+        appointmentDetails: this.appointmentForm.value,
+      };
+
+      // Fetch the doctor ID by name
+      this.http.post(`${this.apiUrl}/send-website-email`, emailRequest)
+              .subscribe({
+                next: (emailResponse) => {
+                  console.log('Email sent successfully:', emailResponse);
+                  this.router.navigate(['/thank-you']);
+                },
+                error: (emailError) => {
+                  console.error('Error sending email:', emailError);
+                },
+              });
     } else {
       this.appointmentForm.markAllAsTouched(); // show all validation messages
     }
