76bd6bc4992832460c3e2b3b565cf11a8c55c756
Rithish06 <rithish.manohar006@gmail.com>
2025-07-09 15:08:37 +0530

    new chagnges and gynecology

--- src/app/ent/ent.component.ts
+++ src/app/ent/ent.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Title,Meta,SafeHtml,DomSanitizer } from '@angular/platform-browser';
+import { Title, Meta, SafeHtml, DomSanitizer } from '@angular/platform-browser';
+declare var gtag: Function;
 
 @Component({
   selector: 'app-ent',
@@ -10,45 +11,46 @@ export class ENTComponent {
   constructor(private titleService: Title, private metaService: Meta, private sanitizer: DomSanitizer) {
 
   }
-  sanitizedContent: SafeHtml ='';
+  sanitizedContent: SafeHtml = '';
   sanitizedContent1: SafeHtml = '';
-  specialities:any[] = [];
+  specialities: any[] = [];
   ngOnInit(): void {
     this.titleService.setTitle("ENT Specialist In Bangalore | ENT Specialist In R R Nagar - Rashtrotthana Hospital");
     this.metaService.updateTag({ name: 'description', content: 'Looking for the best ENT Specialist In R R Nagar or Bangalore? Then look no fur and visit us today. We are the best ENT hospital.' });
-    this.metaService.updateTag({ name: 'keywords', content: 'ENT, ear, nose, throat, sinusitis, ear infections, hearing loss, throat infections, tonsillectomy, sinus surgery, adenoids' 
+    this.metaService.updateTag({
+      name: 'keywords', content: 'ENT, ear, nose, throat, sinusitis, ear infections, hearing loss, throat infections, tonsillectomy, sinus surgery, adenoids'
 
     });
-            
+
     this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content);
     this.sanitizedContent1 = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content_1);
   }
 
   doctors = [
     {
-      doctor_image:'../../assets/Dr-Sunil-Kumar-C.png',
-      doctor_name:'Dr. Sunil Kumar C',
-      experience : "10+",
-      docalt:'Dr. Sunil Kumar C | Best  ENT, Head and Neck Surgeon in Bangalore | Rashtrotthana Hospital'
+      doctor_image: '../../assets/Dr-Sunil-Kumar-C.png',
+      doctor_name: 'Dr. Sunil Kumar C',
+      experience: "10+",
+      docalt: 'Dr. Sunil Kumar C | Best  ENT, Head and Neck Surgeon in Bangalore | Rashtrotthana Hospital'
     },
 
     {
-      doctor_image:'../../assets/Dr-Narendranath-A.png',
-      doctor_name:'Dr. Narendranath A',
-      experience : "10",
-      docalt : 'Dr. Narendranath A | Best ENT Doctor in Bangalore | Rashtrotthana Hospital'
+      doctor_image: '../../assets/Dr-Narendranath-A.png',
+      doctor_name: 'Dr. Narendranath A',
+      experience: "10",
+      docalt: 'Dr. Narendranath A | Best ENT Doctor in Bangalore | Rashtrotthana Hospital'
     },
     {
-      doctor_image:'../../assets/Dr-Manasa-N-A.png',
-      doctor_name:'Dr. Manasa N. A',
-      experience : "7",
-      docalt : 'Dr. Manasa N. A | Best ENT Consultant in Bangalore | Rashtrotthana Hospital'
-    },  
+      doctor_image: '../../assets/Dr-Manasa-N-A.png',
+      doctor_name: 'Dr. Manasa N. A',
+      experience: "7",
+      docalt: 'Dr. Manasa N. A | Best ENT Consultant in Bangalore | Rashtrotthana Hospital'
+    },
     {
-      doctor_image:'../../assets/dr-sandhya-dr-page.png',
-      doctor_name:'Dr. Sandhya S. Patil',
-      experience : "10+",
-      docalt : 'Dr. Sandhya S. Patil | Best ENT Consultant in Bangalore | Rashtrotthana Hospital'
+      doctor_image: '../../assets/dr-sandhya-dr-page.png',
+      doctor_name: 'Dr. Sandhya S. Patil',
+      experience: "10+",
+      docalt: 'Dr. Sandhya S. Patil | Best ENT Consultant in Bangalore | Rashtrotthana Hospital'
     },
   ]
 
@@ -56,45 +58,51 @@ export class ENTComponent {
 
   faqs = [
     {
-      ques : "Who are the Best ENT Specialists in Rajarajeshwari Nagar?",
-      ans : "Rashtrotthana Hospital is home to the Best ENT Specialists in Rajarajeshwari Nagar, including Dr. Manasa N. A., Dr. Narendranath A. and Dr. Sunil Kumar. They specialize in treating conditions such as ear pain, nasal congestion, sinus infections and head and neck cancers treatment in Rajarajeshwari Nagar Bangalore with advanced diagnostic and surgical techniques."
+      ques: "Who are the Best ENT Specialists in Rajarajeshwari Nagar?",
+      ans: "Rashtrotthana Hospital is home to the Best ENT Specialists in Rajarajeshwari Nagar, including Dr. Manasa N. A., Dr. Narendranath A. and Dr. Sunil Kumar. They specialize in treating conditions such as ear pain, nasal congestion, sinus infections and head and neck cancers treatment in Rajarajeshwari Nagar Bangalore with advanced diagnostic and surgical techniques."
     },
     {
-      ques : "Why is Rashtrotthana Hospital considered the Best ENT Hospital in RR Nagar Bangalore?",
-      ans : "Our hospital stands out as the Best ENT Hospital in RR Nagar Bangalore for offering comprehensive care by a team of Expertise ENT doctors in RR Nagar Bangalore. We provide specialized treatments, including endoscopic sinus surgery, snoring treatment ENT services and solutions for throat pain and nasal polyps treatment in RR Nagar Bangalore."
+      ques: "Why is Rashtrotthana Hospital considered the Best ENT Hospital in RR Nagar Bangalore?",
+      ans: "Our hospital stands out as the Best ENT Hospital in RR Nagar Bangalore for offering comprehensive care by a team of Expertise ENT doctors in RR Nagar Bangalore. We provide specialized treatments, including endoscopic sinus surgery, snoring treatment ENT services and solutions for throat pain and nasal polyps treatment in RR Nagar Bangalore."
     },
     {
-      ques : "What treatments are available for ear pain at Rashtrotthana Hospital?",
-      ans : "Our hospital specializes in the treatment for ear pain, including conditions like Otitis Media and Tinnitus. Dr. Manasa N. A. and Dr. Narendranath A. are experts in procedures such as mastoidectomy surgeries, ear tube surgery and micro ear surgery for hearing restoration."
+      ques: "What treatments are available for ear pain at Rashtrotthana Hospital?",
+      ans: "Our hospital specializes in the treatment for ear pain, including conditions like Otitis Media and Tinnitus. Dr. Manasa N. A. and Dr. Narendranath A. are experts in procedures such as mastoidectomy surgeries, ear tube surgery and micro ear surgery for hearing restoration."
     },
     {
-      ques : "Who provides the best care for sinus infections in Rajarajeshwari Nagar?",
-      ans : "Dr. Sunil Kumar is a specialist in sinus infection treatment in Rajarajeshwari Nagar. He performs advanced procedures like endoscopic sinus surgery and FESS (nasal polyp surgery) to provide long-term relief for chronic sinus issues."
+      ques: "Who provides the best care for sinus infections in Rajarajeshwari Nagar?",
+      ans: "Dr. Sunil Kumar is a specialist in sinus infection treatment in Rajarajeshwari Nagar. He performs advanced procedures like endoscopic sinus surgery and FESS (nasal polyp surgery) to provide long-term relief for chronic sinus issues."
     },
     {
-      ques : "Can Rashtrotthana Hospital treat throat-related conditions like chronic coughing?",
-      ans : "Yes, our hospital is recognized as the best hospital for chronic cough treatment in Rajarajeshwari Nagar Bangalore. Dr. Manasa N. A. and Dr. Narendranath A. are experts in treating conditions like tonsillitis, sore throat and throat cancer treatments in Bangalore, ensuring a comprehensive approach to throat care."
+      ques: "Can Rashtrotthana Hospital treat throat-related conditions like chronic coughing?",
+      ans: "Yes, our hospital is recognized as the best hospital for chronic cough treatment in Rajarajeshwari Nagar Bangalore. Dr. Manasa N. A. and Dr. Narendranath A. are experts in treating conditions like tonsillitis, sore throat and throat cancer treatments in Bangalore, ensuring a comprehensive approach to throat care."
     },
     {
-      ques : "What ENT services are available for nasal blockage or nasal discharge?",
-      ans : "Our team, including Dr. Sunil Kumar, specializes in the treatment for nasal congestion, nasal blockage treatment in Bangalore and nasal polyps treatment in RR Nagar Bangalore. Advanced surgical options like nasal septum correction surgery are offered for effective relief."
+      ques: "What ENT services are available for nasal blockage or nasal discharge?",
+      ans: "Our team, including Dr. Sunil Kumar, specializes in the treatment for nasal congestion, nasal blockage treatment in Bangalore and nasal polyps treatment in RR Nagar Bangalore. Advanced surgical options like nasal septum correction surgery are offered for effective relief."
     },
     {
-      ques : "What advanced surgeries are performed at Rashtrotthana Hospital for ENT conditions?",
-      ans : "Our hospital provides cutting-edge ENT procedures such as endoscopic sinus surgery, tracheotomy, adenoidectomy and tonsillectomy. Dr. Narendranath A. is particularly skilled in mastoidectomy surgeries and tympanoplasty, while Dr. Manasa N. A. specializes in voice restoration surgery and head and neck tumor surgeries."
+      ques: "What advanced surgeries are performed at Rashtrotthana Hospital for ENT conditions?",
+      ans: "Our hospital provides cutting-edge ENT procedures such as endoscopic sinus surgery, tracheotomy, adenoidectomy and tonsillectomy. Dr. Narendranath A. is particularly skilled in mastoidectomy surgeries and tympanoplasty, while Dr. Manasa N. A. specializes in voice restoration surgery and head and neck tumor surgeries."
     },
     {
-      ques : "How does Rashtrotthana Hospital treat hearing loss?",
-      ans : "As the best hospital for ENT in Bangalore, we offer advanced care for hearing loss or impairment. Dr. Manasa N. A. and Dr. Sunil Kumar specialize in cochlear implants, micro ear surgery for hearing restoration and treatments for persistent ear aches or pain."
+      ques: "How does Rashtrotthana Hospital treat hearing loss?",
+      ans: "As the best hospital for ENT in Bangalore, we offer advanced care for hearing loss or impairment. Dr. Manasa N. A. and Dr. Sunil Kumar specialize in cochlear implants, micro ear surgery for hearing restoration and treatments for persistent ear aches or pain."
     },
     {
-      ques : "Who provides snoring treatment at Rashtrotthana Hospital?",
-      ans : "Dr. Sunil Kumar is an expert in snoring treatment ENT services and management of obstructive sleep apnea. With advanced diagnostic tools and personalized therapies, he helps patients improve their quality of sleep and overall health."
+      ques: "Who provides snoring treatment at Rashtrotthana Hospital?",
+      ans: "Dr. Sunil Kumar is an expert in snoring treatment ENT services and management of obstructive sleep apnea. With advanced diagnostic tools and personalized therapies, he helps patients improve their quality of sleep and overall health."
     },
     {
-      ques : "What care is available for head and neck cancers in Rajarajeshwari Nagar?",
-      ans : "Dr. Manasa N. A. and Dr. Narendranath A. specialize in head and neck cancer treatment in Rajarajeshwari Nagar Bangalore. Using advanced surgical techniques and personalized care plans, they ensure the best possible outcomes for patients."
+      ques: "What care is available for head and neck cancers in Rajarajeshwari Nagar?",
+      ans: "Dr. Manasa N. A. and Dr. Narendranath A. specialize in head and neck cancer treatment in Rajarajeshwari Nagar Bangalore. Using advanced surgical techniques and personalized care plans, they ensure the best possible outcomes for patients."
     }
   ]
+
+  trackPhoneClick() {
+    gtag('event', 'conversion', {
+      'send_to': 'AW-16656770043/-YEMCITg09IZEPvHyIY-'
+    });
+  }
 
 }
--- src/app/orthopedics/orthopedics.component.ts
+++ src/app/orthopedics/orthopedics.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Title, Meta, DomSanitizer, SafeHtml } from '@angular/platform-browser';
+declare var gtag: Function;
 
 
 @Component({
@@ -26,7 +27,7 @@ export class OrthopedicsComponent {
     this.sanitizedContent1 = this.sanitizer.bypassSecurityTrustHtml(this.specialities[0].content_1);
   }
 
-  formDoctors : any = ['Dr. Mahesh Kulkarni', 'Dr. Sujayendra D. M', 'Dr. Nikhil Hegde', 'Dr. Hemanth Kumar Venkatesh']
+  formDoctors: any = ['Dr. Mahesh Kulkarni', 'Dr. Sujayendra D. M', 'Dr. Nikhil Hegde', 'Dr. Hemanth Kumar Venkatesh']
 
   doctors = [
     {
@@ -40,7 +41,7 @@ export class OrthopedicsComponent {
       doctor_name: 'Dr. Sujayendra D. M',
       experience: "11",
       docalt: 'Dr. Sujayendra D. M | Best Orthopaedic Doctor in Bangalore | Rashtrotthana Hospital'
-    }, 
+    },
     {
 
       doctor_image: '../../assets/Dr-Nikhil-Hegde.png',
@@ -59,57 +60,63 @@ export class OrthopedicsComponent {
 
   faqs = [
     {
-      ques : "What services does the Best Orthopedic Hospital in RR Nagar Bangalore offer?",
-      ans : "At Rashtrotthana Hospital, recognized as the Best Orthopedic Hospital in RR Nagar Bangalore, we provide comprehensive services including joint replacement surgeries, minimally invasive spine surgeries, arthroscopy, fracture care and specialized treatments for various musculoskeletal conditions."
+      ques: "What services does the Best Orthopedic Hospital in RR Nagar Bangalore offer?",
+      ans: "At Rashtrotthana Hospital, recognized as the Best Orthopedic Hospital in RR Nagar Bangalore, we provide comprehensive services including joint replacement surgeries, minimally invasive spine surgeries, arthroscopy, fracture care and specialized treatments for various musculoskeletal conditions."
     },
     {
-      ques : "How can I find an Orthopedic Specialist in RR Nagar?",
-      ans : "You can find experienced Orthopedic Specialists in RR Nagar at Rashtrotthana Hospital. Our team comprises highly qualified professionals dedicated to providing personalized care for all orthopedic concerns."
+      ques: "How can I find an Orthopedic Specialist in RR Nagar?",
+      ans: "You can find experienced Orthopedic Specialists in RR Nagar at Rashtrotthana Hospital. Our team comprises highly qualified professionals dedicated to providing personalized care for all orthopedic concerns."
     },
     {
-      ques : "Who are the Best Orthopedic Surgeons in RR Nagar?",
-      ans : "Rashtrotthana Hospital boasts some of the Best Orthopedic Surgeons in RR Nagar, renowned for their expertise in performing complex orthopedic surgeries with high success rates."
+      ques: "Who are the Best Orthopedic Surgeons in RR Nagar?",
+      ans: "Rashtrotthana Hospital boasts some of the Best Orthopedic Surgeons in RR Nagar, renowned for their expertise in performing complex orthopedic surgeries with high success rates."
     },
     {
-      ques : "What conditions do Bone Specialists in RR Nagar treat?",
-      ans : "Our Bone Specialists in RR Nagar diagnose and treat a wide range of conditions, including fractures, osteoporosis, bone infections and tumors, ensuring comprehensive bone health management."
+      ques: "What conditions do Bone Specialists in RR Nagar treat?",
+      ans: "Our Bone Specialists in RR Nagar diagnose and treat a wide range of conditions, including fractures, osteoporosis, bone infections and tumors, ensuring comprehensive bone health management."
     },
     {
-      ques : "How do I locate the Best Orthopedic Hospital near me?",
-      ans : "If you're searching for the Best Orthopedic Hospital near you, Rashtrotthana Hospital in RR Nagar is a leading choice, offering state-of-the-art facilities and a team of expert orthopedic professionals."
+      ques: "How do I locate the Best Orthopedic Hospital near me?",
+      ans: "If you're searching for the Best Orthopedic Hospital near you, Rashtrotthana Hospital in RR Nagar is a leading choice, offering state-of-the-art facilities and a team of expert orthopedic professionals."
     },
     {
-      ques : "Where can I find the Best Orthopedic Doctors near me?",
-      ans : "Rashtrotthana Hospital in RR Nagar is home to some of the Best Orthopedic Doctors near you, providing exceptional care for all orthopedic conditions."
+      ques: "Where can I find the Best Orthopedic Doctors near me?",
+      ans: "Rashtrotthana Hospital in RR Nagar is home to some of the Best Orthopedic Doctors near you, providing exceptional care for all orthopedic conditions."
     },
     {
-      ques : "How can I consult an orthopedic doctor near me?",
-      ans : "To consult an orthopedic specialist near you, visit Rashtrotthana Hospital in RR Nagar, where our team is ready to assist with your musculoskeletal health needs."
+      ques: "How can I consult an orthopedic doctor near me?",
+      ans: "To consult an orthopedic specialist near you, visit Rashtrotthana Hospital in RR Nagar, where our team is ready to assist with your musculoskeletal health needs."
     },
     {
-      ques : "What should I consider when choosing an orthopedic surgeon?",
-      ans : "When selecting an orthopedic surgeon, consider their qualifications, experience, specialization and patient reviews. Rashtrotthana Hospital's orthopedic surgeons are highly esteemed in these aspects."
+      ques: "What should I consider when choosing an orthopedic surgeon?",
+      ans: "When selecting an orthopedic surgeon, consider their qualifications, experience, specialization and patient reviews. Rashtrotthana Hospital's orthopedic surgeons are highly esteemed in these aspects."
     },
     {
-      ques : "How do I find an orthopedic surgeon near me?",
-      ans : "For those seeking an orthopedic surgeon nearby, Rashtrotthana Hospital in RR Nagar offers access to top-tier surgeons specializing in various orthopedic procedures."
+      ques: "How do I find an orthopedic surgeon near me?",
+      ans: "For those seeking an orthopedic surgeon nearby, Rashtrotthana Hospital in RR Nagar offers access to top-tier surgeons specializing in various orthopedic procedures."
     },
     {
-      ques : "What does a bone doctor do?",
-      ans : "A bones doctor, or orthopedic specialist, diagnoses and treats conditions related to the skeletal system, including fractures, bone diseases and joint disorders."
+      ques: "What does a bone doctor do?",
+      ans: "A bones doctor, or orthopedic specialist, diagnoses and treats conditions related to the skeletal system, including fractures, bone diseases and joint disorders."
     },
     {
-      ques : "Who is the Best Knee Replacement Surgeon in RR Nagar?",
-      ans : "Rashtrotthana Hospital is proud to have some of the Best Knee Replacement Surgeons in RR Nagar, offering advanced surgical techniques for optimal patient outcomes."
+      ques: "Who is the Best Knee Replacement Surgeon in RR Nagar?",
+      ans: "Rashtrotthana Hospital is proud to have some of the Best Knee Replacement Surgeons in RR Nagar, offering advanced surgical techniques for optimal patient outcomes."
     },
     {
-      ques : "What makes Rashtrotthana Hospital the Best Bone Hospital in RR Nagar Bangalore?",
-      ans : "Rashtrotthana Hospital is considered the Best Bone Hospital in RR Nagar Bangalore due to our comprehensive bone health services, advanced technology and a team of expert specialists dedicated to patient care."
+      ques: "What makes Rashtrotthana Hospital the Best Bone Hospital in RR Nagar Bangalore?",
+      ans: "Rashtrotthana Hospital is considered the Best Bone Hospital in RR Nagar Bangalore due to our comprehensive bone health services, advanced technology and a team of expert specialists dedicated to patient care."
     },
     {
-      ques : "Which is the Best Bone Surgery Hospital in RR Nagar Bangalore?",
-      ans : "Rashtrotthana Hospital stands out as the Best Bone Surgery Hospital in RR Nagar Bangalore, equipped with state-of-the-art surgical facilities and a team of highly skilled surgeons."
+      ques: "Which is the Best Bone Surgery Hospital in RR Nagar Bangalore?",
+      ans: "Rashtrotthana Hospital stands out as the Best Bone Surgery Hospital in RR Nagar Bangalore, equipped with state-of-the-art surgical facilities and a team of highly skilled surgeons."
     },
   ]
+
+  trackPhoneClick() {
+    gtag('event', 'conversion', {
+      'send_to': 'AW-16656770043/-YEMCITg09IZEPvHyIY-'
+    });
+  }
 
 }
