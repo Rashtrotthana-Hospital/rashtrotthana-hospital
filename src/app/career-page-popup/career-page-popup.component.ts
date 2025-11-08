a26db0c2c649d6f72de6d5b045c4edf53302dc9f
Rithish06 <rithish.manohar006@gmail.com>
2025-11-08 15:50:50 +0530

    care page pop up

--- src/app/career-page-popup/career-page-popup.component.ts
+++ src/app/career-page-popup/career-page-popup.component.ts
@@ -7,14 +7,28 @@ import { Component } from '@angular/core';
 })
 export class CareerPagePopupComponent {
   showPopup: boolean = true;
+  showPopup2: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
     this.showPopup = true; // Show popup on page load
+
+    setTimeout(() => {
+      this.showPopup = false
+    }, 7000)
+
+    setTimeout(() => {
+      this.showPopup = false
+      this.showPopup2 = true;
+    }, 9000)
   }
 
   closePopup(): void {
     this.showPopup = false; // Hide popup when close button is clicked
+  }
+
+  closePopup2() : void {
+    this.showPopup2 = false;
   }
 }
