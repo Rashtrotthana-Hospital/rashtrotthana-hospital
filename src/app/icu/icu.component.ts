c8563056080c600ae5d44fade898aa1c8780fa6b
Govindaraj606 <govindarajk8092@gmail.com>
2026-03-11 11:45:46 +0530

    next 4 facility pages

--- src/app/icu/icu.component.ts
+++ src/app/icu/icu.component.ts
@@ -1,6 +1,7 @@
 
-import { Component, OnInit } from '@angular/core';
-import { Title, Meta,DomSanitizer,SafeHtml } from '@angular/platform-browser'; 
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Title, Meta, DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { content } from 'html2canvas/dist/types/css/property-descriptors/content';
 
 
@@ -10,35 +11,125 @@ import { content } from 'html2canvas/dist/types/css/property-descriptors/content
   styleUrl: './icu.component.css'
 })
 export class IcuComponent {
-  constructor(private titleService: Title, private metaService: Meta, private sanitizer: DomSanitizer) {
-    
+  constructor(private titleService: Title, private metaService: Meta, private sanitizer: DomSanitizer, private router: Router) {
+
   }
-  sanitizedContent: SafeHtml ='';
+  sanitizedContent: SafeHtml = '';
   sanitizedContent1: SafeHtml = '';
-  facilities:any[] = [];
+  facilities: any[] = [];
   ngOnInit(): void {
-    this.titleService.setTitle("Premier Intensive Care Unit in Bangalore - Rashtrotthana Hospital");  
+    this.titleService.setTitle("Premier Intensive Care Unit in Bangalore - Rashtrotthana Hospital");
 
-  this.metaService.updateTag({ name: 'description', content: 'Get advanced critical care services at Rashtrotthana Hospital\'s top-rated Intensive Care Unit in Bangalore.' });
+    this.metaService.updateTag({ name: 'description', content: 'Get advanced critical care services at Rashtrotthana Hospital\'s top-rated Intensive Care Unit in Bangalore.' });
 
-  this.metaService.updateTag({ name: 'keywords', content: 'intensive care unit, critical care, ICU Bangalore, multispeciality hospital' });
+    this.metaService.updateTag({ name: 'keywords', content: 'intensive care unit, critical care, ICU Bangalore, multispeciality hospital' });
 
-  
-  this.facilities=[
+
+    this.facilities = [
+      {
+        main_heading: 'I.C.U',
+        heading: 'I.C.U',
+        content: 'Rashtrotthana Hospital’s state-of-the-art <a  target="_blank" href="https://en.wikipedia.org/wiki/Intensive_care_unit">Intensive Care Unit (ICU)</a> in Bangalore is equipped with over 30 specialized beds, supporting comprehensive critical care for various medical emergencies. Our ICU facility includes dedicated units: Medical ICU, Surgical ICU, Pediatric ICU, Neonatal ICU and Cardiac ICU, each designed for focused, specialized care with advanced life-support systems and continuous monitoring.',
+        content_1: 'Under the 24/7 guidance of qualified and experienced intensivists, Rashtrotthana Hospital’s ICU is prepared for complex medical needs, providing high-quality emergency care that patients and families can trust. With cutting-edge equipment, strict infection control and round-the-clock staffing, our ICU is a leading choice for critical care in Bangalore.',
+        image_1: 'best-multispeciality-ICU-in-bangalore-rashtrotthana-hospital.jpeg',
+        image_2: 'advanced-critical-care-at-rashtrotthana-hospital.jpeg',
+        bg_image: 'advanced-critical-care-at-rashtrotthana-hospital.png',
+        alt1: 'Rashtrotthana Hospital Intensive Care Unit',
+        alt2: 'Best Multispeciality ICU in Bangalore - Rashtrotthana Hospital',
+        alt3: 'Advanced Critical Care at Rashtrotthana Hospital'
+      }
+    ];
+    this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.facilities[0].content);
+    this.sanitizedContent1 = this.sanitizer.bypassSecurityTrustHtml(this.facilities[0].content_1);
+  }
+
+  chooseCards = [
     {
-      main_heading:'I.C.U',
-      heading:'I.C.U',
-      content:'Rashtrotthana Hospital’s state-of-the-art <a  target="_blank" href="https://en.wikipedia.org/wiki/Intensive_care_unit">Intensive Care Unit (ICU)</a> in Bangalore is equipped with over 30 specialized beds, supporting comprehensive critical care for various medical emergencies. Our ICU facility includes dedicated units: Medical ICU, Surgical ICU, Pediatric ICU, Neonatal ICU and Cardiac ICU, each designed for focused, specialized care with advanced life-support systems and continuous monitoring.',
-      content_1:'Under the 24/7 guidance of qualified and experienced intensivists, Rashtrotthana Hospital’s ICU is prepared for complex medical needs, providing high-quality emergency care that patients and families can trust. With cutting-edge equipment, strict infection control and round-the-clock staffing, our ICU is a leading choice for critical care in Bangalore.',
-      image_1:'best-multispeciality-ICU-in-bangalore-rashtrotthana-hospital.jpeg',
-      image_2:'advanced-critical-care-at-rashtrotthana-hospital.jpeg',
-      bg_image:'advanced-critical-care-at-rashtrotthana-hospital.png',
-      alt1 : 'Rashtrotthana Hospital Intensive Care Unit',
-      alt2 : 'Best Multispeciality ICU in Bangalore - Rashtrotthana Hospital',
-      alt3 : 'Advanced Critical Care at Rashtrotthana Hospital'
-    }
+      img: 'assets/facility/modern-medicine.png',
+      heading: 'Modern Medicine (Allopathy)',
+      para: 'Evidence-based clinical care with advanced diagnostics and treatment protocols for acute and chronic conditions.'
+    },
+    {
+      img: 'assets/facility/ayurveda.png',
+      heading: 'Ayurveda',
+      para: 'Personalized herbal therapies, detoxification, and preventive wellness programs that restore balance and promote natural healing.'
+    },
+    {
+      img: 'assets/facility/homeopathy.png',
+      heading: 'Homeopathy',
+      para: 'Gentle, patient-centric remedies addressing the root cause of illness while supporting overall physical and emotional well-being.'
+    },
+    {
+      img: 'assets/facility/yoga.png',
+      heading: 'Yoga & Lifestyle Therapy',
+      para: 'Therapeutic yoga, breathwork, and lifestyle guidance to enhance flexibility, mental clarity and stress resilience.'
+    },
+    {
+      img: 'assets/facility/naturopathy.png',
+      heading: 'Naturopathy ',
+      para: 'Natural therapies combined with expert nutritional guidance to strengthen immunity, correct metabolic imbalances, and support long-term wellness.'
+    },
+  ]
+
+  images = [
+    '/assets/facility/facility-img-6.jpg',
+    '/assets/facility/facility-img-16.jpg',
+    '/assets/facility/facility-img-13.jpg',
+    '/assets/facility/facility-img-12.jpg',
+    '/assets/facility/facility-img-3.jpg',
+    '/assets/facility/facility-img-2.jpg'
   ];
-  this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.facilities[0].content);
-  this.sanitizedContent1 = this.sanitizer.bypassSecurityTrustHtml(this.facilities[0].content_1);
-}
+
+  @ViewChild('track', { static: true }) track!: ElementRef;
+
+
+  currentIndex = 0;
+  cardWidth = 250;
+  gap = 20;
+
+  next() {
+    const maxIndex = this.images.length - this.visibleCards();
+    if (this.currentIndex < maxIndex) {
+      this.currentIndex++;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  visibleCards() {
+    const containerWidth = window.innerWidth;
+    return Math.floor(containerWidth / (this.cardWidth + this.gap));
+  }
+
+  getTransform() {
+    return `translateX(-${this.currentIndex * (this.cardWidth + this.gap)}px)`;
+  }
+
+
+  cards = [
+    { title: 'Insurance', icon: '/assets/facility/img-1.png', route: '/facility/health-insurance-plans-bangalore' },
+    { title: 'Emergency & Trauma Care', icon: '/assets/facility/img-2.png', route: '/facility/best-emergency-trauma-multispeciality-hospital-bangalore' },
+    { title: 'In-patient facility', icon: '/assets/facility/img-3.png', route: '/facility/inpatient-facility-bangalore' },
+    { title: 'Dialysis Unit', icon: '/assets/facility/img-4.png', route: '/facility/best-kidney-dialysis-multispeciality-hospital-bangalore' },
+    { title: 'Operation Theatre', icon: '/assets/facility/img-5.png', route: '/facility/operation-theatre-bangalore' },
+    { title: 'ICU', icon: '/assets/facility/img-6.png', route: '/facility/best-intensive-care-unit-multispeciality-hospital-bangalore' },
+    { title: 'Radiology Services', icon: '/assets/facility/img-7.png', route: '/facility/top-diagnostics-multi-speciality-hospital-bangalore' },
+    { title: 'Pharmacy', icon: '/assets/facility/img-8.png', route: '/facility/24-hours-pharmacy-store-bangalore' },
+    { title: 'Ambulance Services', icon: '/assets/facility/img-9.png', route: '/facility/best-ambulance-service-rashtrotthana-hospital-bangalore' },
+    { title: 'Dietary', icon: '/assets/facility/img-10.png', route: '/facility/dietary-services-bangalore' },
+    { title: 'Physiotherapy', icon: '/assets/facility/img-11.png', route: '/facility/physiotherapy-services-bangalore' },
+    { title: 'Laboratory Services', icon: '/assets/facility/img-12.png', route: '/facility/laboratory-services-bangalore' },
+    { title: 'Endoscopy', icon: '/assets/facility/img-13.png', route: '/facility/endoscopy-services-bangalore' },
+    { title: 'Nutrition & Dietetics', icon: '/assets/facility/img-14.png', route: '/facility/nutrition-dietetics-services-bangalore' },
+
+  ];
+
+
+  goToPage(route: string) {
+    this.router.navigate([route]);
+  }
 }
